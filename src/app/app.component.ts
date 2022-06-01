e028c6e3900f7f4c830a75d8d7b0f2a2724629dc
cagkanoztas <cagkan.oztas@jengal.com>
2022-06-01 16:26:27 +0300

    angular 13 material theming completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, Inject, OnInit, Renderer2} from '@angular/core';
+import {MatSelectChange} from '@angular/material/select';
+import {DOCUMENT} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-material-theming-class13';
+export class AppComponent implements OnInit {
+  readonly themeAnchor = this.document.getElementById('app-theme');
+
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private renderer: Renderer2
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.setLightTheme();
+  }
+
+  setTheme({source}: MatSelectChange): void {
+    source.value === 'dark'
+      ? this.setDarkTheme()
+      : this.setLightTheme();
+  }
+
+  setDarkTheme(): void {
+    this.renderer.setAttribute(this.themeAnchor, 'href', '/dark-theme.css');
+  }
+
+  setLightTheme(): void {
+    this.renderer.setAttribute(this.themeAnchor, 'href', '/light-theme.css');
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedMaterialModule } from './shared-material.module';
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    SharedMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
